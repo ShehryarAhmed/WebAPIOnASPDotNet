78dc87e3dbda9d48e2ceb3588a1a181977300d60
shehryarahmed <shehreyarahmed5@gmail.com>
2018-03-30 17:18:57 +0500

    implemented post method'

--- Users/Users/Controllers/UsersController.cs
+++ Users/Users/Controllers/UsersController.cs
@@ -25,5 +25,23 @@ namespace Users.Controllers
                 return entities.usersTables.FirstOrDefault(e => e.userID == id);
             }
         }
+
+        public HttpResponseMessage Post([FromBody]usersTable users) {
+
+            try
+            {
+                using (shehryarTestingDBEntities entities = new shehryarTestingDBEntities())
+                {
+                    entities.usersTables.Add(users);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, users);
+                    message.Headers.Location = new Uri(Request.RequestUri + users.userID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex) {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
     }
 }
